9a002d2317430acde36ad805a554e334085cfab5
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-06 10:49:01 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,23 @@
-const decBtn = document.querySelector('#counter button[data-action="decrement"]');
-const incBtn = document.querySelector('#counter button[data-action="increment"]');
-const resSpan = document.querySelector('#value');
-let counterValue = 0;
-decBtn.addEventListener('click', () => {--counterValue });
-incBtn.addEventListener('click', () => {++counterValue});
-console.log(counterValue);
+const refs = {
+    decrementBtn: document.querySelector('button[data-action="decrement"]'),
+    incrementBtn: document.querySelector('button[data-action="increment"]'),
+    valueSpan: document.querySelector('#value')
+};
 
-resSpan.textContent = counterValue
+const clickFunctions = {
+    value: 0,
+    incMetod(){
+        this.value -= 1;
+        return refs.valueSpan.textContent = this.value
+    },
+    decMetod(){
+        this.value += 1;
+        return refs.valueSpan.textContent = this.value
+    }
+};
+
+
+
+
+refs.decrementBtn.addEventListener('click', clickFunctions.incMetod.bind(clickFunctions));
+refs.incrementBtn.addEventListener('click', clickFunctions.decMetod.bind(clickFunctions));
