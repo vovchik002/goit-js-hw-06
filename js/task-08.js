6405f12963ccc3a96fccf94c199a32b9f11911b0
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-06 21:49:43 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+
+
+form.addEventListener("submit", testForm);
+
+function testForm(event) {
+    event.preventDefault();
+
+const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    data.email = data.email.trim().toLowerCase();
+  for (const [key, value] of Object.entries(data)) {
+      if (value !== '') continue;
+      alert('Всі поля повинні бути заповнені!');
+      return;
+    }
+    console.log(data);
+}
 
