c8e4c4cdbae3cde13b28ad88761b4593c82f957c
vovchik002 <106514220+vovchik002@users.noreply.github.com>
2022-11-06 21:21:07 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const backGroundColor = document.querySelector("body");
+const buttonChange = document.querySelector("button.change-color");
+const spanColor = document.querySelector("span.color")
+buttonChange.addEventListener("click", changeColorFn);
+
+function changeColorFn() {
+  backGroundColor.style.backgroundColor = getRandomHexColor()
+  spanColor.textContent = getRandomHexColor()
+
+};
